36fae25a748fbedab292eca83d0fa581a69ce80c
ClaytonFrandeen <157328251+ClaytonFrandeen@users.noreply.github.com>
2026-03-05 19:12:30 -0800

    Added ShopUI
    Added a shop UI
    Added triggercolliders to player controller to find interactables.
    Added IInteractable.cs
    Added IUI.cs
    Added ShopInteractable.cs

--- Assets/Scripts/Character/PlayerController.cs
+++ Assets/Scripts/Character/PlayerController.cs
@@ -1,8 +1,8 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.EventSystems;
 using Farming;
 using System.Collections.Generic;
-
 namespace Character 
 {
     [RequireComponent(typeof(PlayerInput))]
@@ -10,15 +10,20 @@ namespace Character
     public class PlayerController : MonoBehaviour
     {
         [SerializeField] private TileSelector tileSelector;
+        private IUI currentUI;
         private MovementController moveController;
         private AnimatedController animatedController;
         private PlayerFarming playerFarming;
+
+        public IInteractable currentInteractable;
+        public bool canMove = true;
 
         void Start()
         {
             moveController = GetComponent<MovementController>();
             animatedController = GetComponent<AnimatedController>();
             playerFarming = GetComponent<PlayerFarming>();
+            currentUI = GetComponent<IUI>();
 
             Debug.Assert(moveController, "PlayerController requires a MovementController");
             Debug.Assert(playerFarming, "PlayerController requires PlayerFarming script");
@@ -27,24 +32,56 @@ namespace Character
 
         public void OnMove(InputValue inputValue)
         {
+            if(!canMove) {return;}; // disables player movement for inside UI menus
             Vector2 inputVector = inputValue.Get<Vector2>();
             moveController.Move(inputVector);
         }
 
         public void OnJump(InputValue inputValue)
         {
+            if(!canMove) return;   // disable player movement for inside UI menus
             moveController.Jump();
             if(animatedController) animatedController.Jump();
         }
         
         public void OnInteract(InputValue value)
         {
+
+            // This if block stops left click from hitting any ui interactables
+            if (currentUI != null && currentUI.getOpen()) {      
+                
+            }
+            if (currentInteractable != null)
+            {
+                currentInteractable.Interact();
+                return;
+            }
             // Delegate the logic to the farming script
             FarmTile tile = tileSelector.GetSelectedTile(); //you HAVE to make sure the tile selector is set to player (raycaster) in the inspector!
             //Debug.Log(tileSelector.GetSelectedTile().gameObject.name + " recognized for OnInteract");
             List<IWaterable> waterable = tileSelector.GetSelectionOfType<IWaterable>();
             //Debug.Log(waterable);
             playerFarming.AttemptInteraction(tile);
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            IInteractable interactable = other.GetComponent<IInteractable>();
+
+            if (interactable != null)
+            {
+                currentInteractable = interactable;
+            }
+        }
+
+        private void OnTriggerExit(Collider other)
+        {
+            IInteractable interactable = other.GetComponent<IInteractable>();
+
+            if (interactable != null && interactable == currentInteractable)
+            {
+                currentInteractable = null;
+            }            
         }
     }
 }
